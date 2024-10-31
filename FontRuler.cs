484971d99ff2d6b3d0a31d459cb23439b7903b72
Eduard Lepner <eduard@invarion.com>
2024-10-31 11:12:14 +0100

    reproduce crash

--- FontRuler.cs
+++ FontRuler.cs
@@ -1,40 +1,40 @@
-﻿//using System.Linq;
-//using SkiaSharp;
-//using SkiaSharp.HarfBuzz;
+﻿using System.Linq;
+using SkiaSharp;
+using SkiaSharp.HarfBuzz;
 
-//namespace blazor9;
+namespace blazor9;
 
-//public class FontRuler : IFontRuler
-//{
-//    private static IFontRuler? _instance = null;
+public class FontRuler : IFontRuler
+{
+    private static IFontRuler? _instance = null;
 
-//    /// <summary>
-//    /// An external font ruler can be set here to override the default.
-//    /// </summary>
-//    public static IFontRuler Instance
-//    {
-//        get => _instance ??= new FontRuler();
+    /// <summary>
+    /// An external font ruler can be set here to override the default.
+    /// </summary>
+    public static IFontRuler Instance
+    {
+        get => _instance ??= new FontRuler();
 
-//        set => _instance = value;
-//    }
-
-
-
-//    public string[] AvailableFonts { get; } = SKFontManager.Default.FontFamilies.Where(x =>
-//        {
-//            using (var typeFace = SKTypeface.FromFamilyName(x))
-//            using (var shaper = new SKShaper(typeFace))
-//                return shaper.Typeface.GetType().Name != null;
-//        })
-//        .OrderBy(x => x)
-//        .ToArray();
-//}
+        set => _instance = value;
+    }
 
 
-//public interface IFontRuler
-//{
-//    string[] AvailableFonts { get; }
-//}
+
+    public string[] AvailableFonts { get; } = SKFontManager.Default.FontFamilies.Where(x =>
+        {
+            using (var typeFace = SKTypeface.FromFamilyName(x))
+            using (var shaper = new SKShaper(typeFace))
+                return shaper.Typeface.GetType().Name != null;
+        })
+        .OrderBy(x => x)
+        .ToArray();
+}
+
+
+public interface IFontRuler
+{
+    string[] AvailableFonts { get; }
+}
 
 public class Hello
 {
